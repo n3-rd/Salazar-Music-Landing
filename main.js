e56e768ad227444da91ee0307911676e7d72f61a
n3-rd <godwinjdayo@gmail.com>
2022-10-24 02:18:22 +0100

    formatted code for better readability

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-import './style.scss'
-import luge from '@waaark/luge'
-import Lenis from '@studio-freight/lenis'
+import "./style.scss";
+import luge from "@waaark/luge";
+import Lenis from "@studio-freight/lenis";
 
-const smoothScroll = ()=>{
+const smoothScroll = () => {
   const lenis = new Lenis({
     duration: 1.2,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-    direction: 'vertical', // vertical, horizontal
-    gestureDirection: 'vertical', // vertical, horizontal, both
+    direction: "vertical", // vertical, horizontal
+    gestureDirection: "vertical", // vertical, horizontal, both
     smooth: true,
     mouseMultiplier: 1,
     smoothTouch: false,
     touchMultiplier: 2,
     infinite: false,
-  })
+  });
 
   function raf(time) {
-    lenis.raf(time)
-    requestAnimationFrame(raf)
+    lenis.raf(time);
+    requestAnimationFrame(raf);
   }
 
-  requestAnimationFrame(raf)
-}
+  requestAnimationFrame(raf);
+};
 
-document.addEventListener('DOMContentLoaded', () => {
-  smoothScroll()
-})
+document.addEventListener("DOMContentLoaded", () => {
+  smoothScroll();
+});
